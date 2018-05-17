506da4f1f8d408b69c5a96ce0ce17742c7833758
Christian Häusler <christian.haeusler@prodyna.com>
2018-05-17 16:48:42 +0200

    added css

--- src/components/puristic.ts
+++ src/components/puristic.ts
@@ -1,7 +1,13 @@
-import {Component} from '../component';
+import { Component } from '../component';
+import './puristic.css';
 
 export default class Puristic implements Component {
     render(container: HTMLElement, options?: any): void {
-        throw new Error("Method not implemented.");
+        container.classList.add('puristic');
+
+        const h1 = document.createElement('h1');
+        h1.innerHTML = "Hello Puristic";
+
+        container.appendChild(h1);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ComponentRegistry, loadComponents } from './component-loader';
 
+import './theme.css'
+
 const componentRegistry: ComponentRegistry = {
     'puristic': () => import('./components/puristic')
 };
